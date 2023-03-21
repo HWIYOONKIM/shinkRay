2a424873be9b6db11f09aabe1ae0e0feaa46d90f
hwiyoon_kim <hwiyoon.kim@smail.astate.edu>
2023-03-20 23:08:07 -0500

    Entity

--- src/types/express-session.d.ts
+++ src/types/express-session.d.ts
@@ -5,6 +5,8 @@ declare module 'express-session' {
     clearSession(): Promise<void>; // DO NOT MODIFY THIS!
 
     // NOTES: Add your app's custom session properties here:
-   
+    authenticatedUser: {
+      userId: string;
+    };
   }
 }
